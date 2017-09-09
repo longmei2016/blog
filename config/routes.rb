c6ce2c8ac1057ebf5c63195ac6c491fbe7380ce2
ankon <ankon@ankon-mac.local>
2017-09-09 15:38:35 +0800

    add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  
+
   resources :posts do
     resources :comments
   end
 
   root "posts#index"
+
+  get '/about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
